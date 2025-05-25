a13035f019a7d7d3137077768e606cac586cc0f1
Gabriel <ge326@uowmail.edu.au>
2025-05-25 22:22:17 +1000

    Update client-storage-wrapper.js
    Signed-off-by: Gabriel <ge326@uowmail.edu.au>

--- src/public/js/client-storage-wrapper.js
+++ src/public/js/client-storage-wrapper.js
@@ -1,4 +1,7 @@
 class ClientStorageWrapper {
+    static _dbName = 'FlyDreamAirStorage';
+    static _storeName = 'store';
+
     static _canUse(type) {
         try {
             const s = window[`${type}Storage`];
@@ -20,9 +23,6 @@ class ClientStorageWrapper {
             return false;
         }
     }
-
-    static _dbName = 'FlyDreamAirStorage';
-    static _storeName = 'store';
 
     static async _getDB() {
         return new Promise((resolve, reject) => {
@@ -46,7 +46,6 @@ class ClientStorageWrapper {
         const db = await this._getDB();
         const tx = db.transaction(this._storeName, 'readonly');
         const store = tx.objectStore(this._storeName);
-
         return new Promise((resolve, reject) => {
             const request = store.get(key);
             request.onsuccess = () => resolve(request.result);
@@ -70,28 +69,29 @@ class ClientStorageWrapper {
 
     static _setCookie(k, v, d, secure = false) {
         const exp = new Date(Date.now() + d * 864e5).toUTCString();
-        const secureAttr = secure ? '; Secure; SameSite=Strict' : '; SameSite=Strict';
-        document.cookie = `${encodeURIComponent(k)}=${encodeURIComponent(v)}; expires=${exp}; path=/` + secureAttr;
+        const secureFlag = secure || location.protocol === 'https:';
+        const secureAttr = secureFlag ? '; Secure; SameSite=Lax' : '; SameSite=Lax';
+        document.cookie = `${encodeURIComponent(k)}=${encodeURIComponent(v)}; expires=${exp}; path=/${secureAttr}`;
     }
 
     static _getCookie(k) {
-        const cookies = document.cookie.split('; ');
+        const name = encodeURIComponent(k) + "=";
+        const cookies = decodeURIComponent(document.cookie).split('; ');
         for (const c of cookies) {
-            const index = c.indexOf('=');
-            if (index === -1) continue;
-            const key = decodeURIComponent(c.slice(0, index));
-            const value = decodeURIComponent(c.slice(index + 1));
-            if (key === k) return value;
+            if (c.startsWith(name)) return c.slice(name.length);
         }
         return null;
     }
 
     static _removeCookie(k) {
-        document.cookie = `${encodeURIComponent(k)}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict`;
+        document.cookie = `${encodeURIComponent(k)}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Lax`;
     }
 
     static _clearCookies() {
-        document.cookie.split('; ').forEach(c => this._removeCookie(c.split('=')[0]));
+        document.cookie.split('; ').forEach(c => {
+            const key = c.split('=')[0];
+            this._removeCookie(key);
+        });
     }
 
     static async set(key, val, type = 'local', opts = {}) {
@@ -112,9 +112,13 @@ class ClientStorageWrapper {
         if (type === 'indexed') {
             return await this._idbGet(key);
         }
-        if (type === 'local' && this._canUse('local')) val = localStorage.getItem(key);
-        else if (type === 'session' && this._canUse('session')) val = sessionStorage.getItem(key);
-        else if (type === 'cookie' && this._cookiesOn()) val = this._getCookie(key);
+        if (type === 'local' && this._canUse('local')) {
+            val = localStorage.getItem(key);
+        } else if (type === 'session' && this._canUse('session')) {
+            val = sessionStorage.getItem(key);
+        } else if (type === 'cookie' && this._cookiesOn()) {
+            val = this._getCookie(key);
+        }
 
         try {
             return JSON.parse(decodeURIComponent(val));
@@ -144,12 +148,11 @@ class ClientStorageWrapper {
 
     static async getAllIndexedDBItems() {
         return new Promise((resolve, reject) => {
-            const request = indexedDB.open('FlyDreamAirStorage', 1);
+            const request = indexedDB.open(this._dbName, 1);
             request.onsuccess = () => {
                 const db = request.result;
-                const tx = db.transaction('store', 'readonly');
-                const store = tx.objectStore('store');
-
+                const tx = db.transaction(this._storeName, 'readonly');
+                const store = tx.objectStore(this._storeName);
                 const allData = {};
                 const cursorRequest = store.openCursor();
 
@@ -165,7 +168,6 @@ class ClientStorageWrapper {
 
                 cursorRequest.onerror = () => reject(cursorRequest.error);
             };
-
             request.onerror = () => reject(request.error);
         });
     }
